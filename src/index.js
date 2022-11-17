0989a0f85bef3b2e00bbffc54584b3082f6fc5fc
Alejandra Franco <alejandra.franco@sony.com>
2022-11-17 05:18:51 -0800

    add scripts to index.html

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import ArticleList from './features/articles/articleList';
 import App from './App';
-import './src/index.css';
+import './index.css';
 
 
 const container = document.getElementById('root');
