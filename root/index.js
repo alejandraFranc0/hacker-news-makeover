9c5b194443fb245dd0f51a73660994e2e5804dc4
Alejandra Franco <alejandra.franco@sony.com>
2022-11-17 04:47:57 -0800

    File Structure Changes

--- root/index.js
+++ root/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import ArticleList from './features/articles/articleList';
-import App from './App';
-import './index.css';
+import { store } from '../src/app/store';
+import ArticleList from '../src/features/articles/articleList';
+import App from '../src/App';
+import './src/index.css';
 
 
 const container = document.getElementById('root');
